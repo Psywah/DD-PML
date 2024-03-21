e4989067d9a6c13bf55c890bb048ea3221118555
Shihua Gong <psywah@gmail.com>
2024-03-22 00:14:14 +0800

    ex2

---
+++ exprs/ex2-2/run_tests_sweep_checker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+# first argument wave number, second argument prec type(sweep )
+echo "Hello World!"
+
+echo "number of subdomain $1"
+#echo "overlap length $2"
+
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6 
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 2 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 4 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 8 -sweep4 -primal -DDS=1 -DNN=$1 -DDA=1 -DPOU=2 -tol 1.e-6
