d4f9ab752b2f1ee5e79dac9e22053041c9c859db
Shihua Gong <psywah@gmail.com>
2024-03-21 10:56:32 +0800

    add ex1-2

---
+++ exprs/ex1-2/run_tests_sweep.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+# first argument wave number, second argument prec type(sweep )
+echo "Hello World!"
+
+echo "number of subdomain $1"
+#echo "overlap length $2"
+
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npart 2 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npart 4 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDD.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npart 8 -sweep4 -primal -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
