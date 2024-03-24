60d4a2421fde60c6458596b2d100e02ec5778dfa
Shihua Gong <psywah@gmail.com>
2024-03-24 15:24:21 +0800

    add

--- exprs/ex1-2/run_tests_sweep_checker.sh
+++ exprs/ex1-2/run_tests_sweep_checker.sh
@@ -5,23 +5,23 @@ echo "Hello World!"
 echo "number of subdomain $1"
 #echo "overlap length $2"
 
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12 
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10 
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 2 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
 
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 4 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
 
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
-FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-12
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 100 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 150 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 200 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 250 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 300 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
+FreeFem++  HelmPMLDDcheckerboard.edp -ns -nw -lengthoverlaps 0.025 -lengthpml 0.025 -gmres 0 -apml 5000 -wavenumber 350 -npartx 8 -sweep33 -primal -DDS=1 -DNN=1 -DDA=1 -DPOU=2 -tol 1.e-10
